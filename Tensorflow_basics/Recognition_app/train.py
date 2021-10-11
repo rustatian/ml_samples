21e0ecf8f88a503d577a8a9ea465118f4baa3fb1
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-10-11 09:13:12 +0300

    Fix some v1->v2 issues
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- Tensorflow_basics/Recognition_app/train.py
+++ Tensorflow_basics/Recognition_app/train.py
@@ -56,19 +56,19 @@ def convert_to_one_hot(Y, C):
 
 
 def create_placeholders(n_x, n_y):
-    X = tf.placeholder(dtype=tf.float32, shape=(n_x, None))
-    Y = tf.placeholder(dtype=tf.float32, shape=(n_y, None))
+    X = tf.compat.v1.placeholder(dtype=tf.float32, shape=(n_x, None))
+    Y = tf.compat.v1.placeholder(dtype=tf.float32, shape=(n_y, None))
 
     return X, Y
 
 
 def initialize_parameters():
-    W1 = tf.get_variable("W1", [25, 12288], initializer=tf.contrib.layers.xavier_initializer())
-    b1 = tf.get_variable("b1", [25, 1], initializer=tf.zeros_initializer())
-    W2 = tf.get_variable("W2", [12, 25], initializer=tf.contrib.layers.xavier_initializer())
-    b2 = tf.get_variable("b2", [12, 1], initializer=tf.zeros_initializer())
-    W3 = tf.get_variable("W3", [6, 12], initializer=tf.contrib.layers.xavier_initializer())
-    b3 = tf.get_variable("b3", [6, 1], initializer=tf.zeros_initializer())
+    W1 = tf.compat.v1.get_variable("W1", [25, 12288], initializer=tf.contrib.layers.xavier_initializer())
+    b1 = tf.compat.v1.get_variable("b1", [25, 1], initializer=tf.zeros_initializer())
+    W2 = tf.compat.v1.get_variable("W2", [12, 25], initializer=tf.contrib.layers.xavier_initializer())
+    b2 = tf.compat.v1.get_variable("b2", [12, 1], initializer=tf.zeros_initializer())
+    W3 = tf.compat.v1.get_variable("W3", [6, 12], initializer=tf.contrib.layers.xavier_initializer())
+    b3 = tf.compat.v1.get_variable("b3", [6, 1], initializer=tf.zeros_initializer())
 
     parameters = {"W1": W1,
                   "b1": b1,
